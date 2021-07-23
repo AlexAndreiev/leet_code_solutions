27f5d40513fd4b5c58bbdf6bde769c467b274084
Oleksand Andreiev <andreyew89gmail.com>
2021-07-23 21:45:43 +0300

    MergeTwoSortedLists_easy_21.java and MergeTwoSortedLists_easy_21Test.java have been added

---
+++ test/com/alex/andreiev/linkedList/MergeTwoSortedLists_easy_21Test.java
@@ -0,0 +1,39 @@
+package com.alex.andreiev.linkedList;
+
+import com.alex.andreiev.utils.ListNode;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MergeTwoSortedLists_easy_21Test {
+
+    @Test
+    void mergeTwoLists() {
+        var l1 = new ListNode(1, new ListNode(2, new ListNode(4)));
+        var l2 = new ListNode(1, new ListNode(3, new ListNode(4)));
+        var target = new MergeTwoSortedLists_easy_21();
+        var list = target.mergeTwoLists(l1, l2);
+        checkList(list, new int[]{1, 1, 2, 3, 4});
+
+        list = target.mergeTwoLists(null, null);
+        checkList(list, null);
+
+        l1 = new ListNode(0);
+        list = target.mergeTwoLists(l1, null);
+        checkList(list, new int[]{0});
+
+    }
+
+    private void checkList(ListNode list, int[] expectedArr) {
+        if (expectedArr == null) {
+            assertNull(list);
+            return;
+        }
+
+        var tmpList = list;
+        for (var val: expectedArr) {
+            assertEquals(val, tmpList.val);
+            tmpList = tmpList.next;
+        }
+    }
+}
