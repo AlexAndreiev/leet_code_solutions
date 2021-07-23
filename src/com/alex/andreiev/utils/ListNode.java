ef86ed9b7a47a993b6cc1cf14df028ce8ccd9d49
Oleksand Andreiev <andreyew89gmail.com>
2021-07-23 21:46:44 +0300

    ListNode: constructor with next parameter has been added

--- src/com/alex/andreiev/utils/ListNode.java
+++ src/com/alex/andreiev/utils/ListNode.java
@@ -1,10 +1,15 @@
 package com.alex.andreiev.utils;
 
+
 public class ListNode {
     public int val;
     public ListNode next;
     public ListNode(int x) {
         val = x;
         next = null;
+    }
+    public ListNode(int x, ListNode next) {
+        val = x;
+        this.next = next;
     }
 }
