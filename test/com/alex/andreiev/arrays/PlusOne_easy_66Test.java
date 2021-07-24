f9c19dafe31cc8ae660ea54dc551a12c1807613c
Oleksand Andreiev <andreyew89gmail.com>
2021-07-24 22:15:04 +0300

    PlusOne_easy_66.java and PlusOne_easy_66Test.java have been added

---
+++ test/com/alex/andreiev/arrays/PlusOne_easy_66Test.java
@@ -0,0 +1,19 @@
+package com.alex.andreiev.arrays;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlusOne_easy_66Test {
+
+    @Test
+    void plusOne() {
+        var target = new PlusOne_easy_66();
+        // The array represents the integer 123.
+        assertArrayEquals(new int[]{1, 2, 4}, target.plusOne(new int[] {1, 2, 3}));
+
+        // The array represents the integer 4321.
+        assertArrayEquals(new int[]{4,3,2,2}, target.plusOne(new int[] {4,3,2,1}));
+        assertArrayEquals(new int[]{1}, target.plusOne(new int[] {0}));
+    }
+}
