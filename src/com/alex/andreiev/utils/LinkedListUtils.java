9d7744bcea9bfb26d538b4718322697a77e449cd
Oleksand Andreiev <andreyew89gmail.com>
2021-07-26 22:10:39 +0300

    LinkedListUtils with isListChecked() method has been added

---
+++ src/com/alex/andreiev/utils/LinkedListUtils.java
@@ -0,0 +1,16 @@
+package com.alex.andreiev.utils;
+
+public class LinkedListUtils {
+
+    public static boolean isListChecked(ListNode expected, ListNode actual) {
+        if (expected == null || actual == null) return false;
+        while (expected != null) {
+            if (actual == null) return false;
+            if (expected.val != expected.val) return false;
+            expected = expected.next;
+            actual = actual.next;
+        }
+        if (actual != null) return false;
+        return true;
+    }
+}
