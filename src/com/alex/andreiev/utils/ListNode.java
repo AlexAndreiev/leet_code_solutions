96a7b9690efa9cf2cb04bd2b73f602fea783595d
Oleksand Andreiev <andreyew89gmail.com>
2021-07-26 22:10:02 +0300

    No argument constructor for ListNode has been added

--- src/com/alex/andreiev/utils/ListNode.java
+++ src/com/alex/andreiev/utils/ListNode.java
@@ -4,6 +4,8 @@ package com.alex.andreiev.utils;
 public class ListNode {
     public int val;
     public ListNode next;
+
+    public ListNode() {}
     public ListNode(int x) {
         val = x;
         next = null;
