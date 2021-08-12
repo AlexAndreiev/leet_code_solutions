a3bbfff383c5f4da618adc73d11782757f1b4cb3
Oleksandr Andreiev <andreyew89@gmail.com>
2021-08-12 17:31:02 +0300

    AddTwoNumbers_medium_2 and AddTwoNumbers_medium_2Test have been added

---
+++ src/com/alex/andreiev/linkedList/AddTwoNumbers_medium_2.java
@@ -0,0 +1,46 @@
+package com.alex.andreiev.linkedList;
+
+import com.alex.andreiev.utils.ListNode;
+
+/*You are given two non-empty linked lists representing two non-negative integers.
+The digits are stored in reverse order, and each of their nodes contains a single digit.
+Add the two numbers and return the sum as a linked list.
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+Constraints:
+    The number of nodes in each linked list is in the range [1, 100].
+    0 <= Node.val <= 9
+    It is guaranteed that the list represents a number that does not have leading zeros.
+*/
+public class AddTwoNumbers_medium_2 {
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        /* Approach 1
+            Time complexity - O(n)
+            Space complexity - O(n+1)
+         */
+
+        if (l1 == null && l2 == null) return null;
+
+        var sum = new ListNode(0);
+        var workPointer = sum;
+        int extra = 0;
+        while (l1 != null || l2 != null) {
+            int tmpSum = extra;
+            if (l1 != null) {
+                tmpSum += l1.val;
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                tmpSum += l2.val;
+                l2 = l2.next;
+            }
+            extra = tmpSum / 10;
+            workPointer.next = new ListNode(tmpSum % 10);
+            workPointer = workPointer.next;
+        }
+        if (extra != 0)
+            workPointer.next = new ListNode(extra);
+        return sum.next;
+    }
+}
---
+++ test/com/alex/andreiev/linkedList/AddTwoNumbers_medium_2Test.java
@@ -0,0 +1,37 @@
+package com.alex.andreiev.linkedList;
+
+
+import com.alex.andreiev.utils.LinkedListUtils;
+import com.alex.andreiev.utils.ListNode;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AddTwoNumbers_medium_2Test {
+
+    @Test
+    void addTwoNumbers() {
+        var target = new AddTwoNumbers_medium_2();
+
+        var l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
+        var l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
+//        Explanation: 342 + 465 = 807.
+        var expected = new ListNode(7, new ListNode(0, new ListNode(8)));
+        var actual = target.addTwoNumbers(l1, l2);
+        assertTrue(LinkedListUtils.isLinkedListEqual(expected, actual));
+
+        l1 = new ListNode(0);
+        l2 = new ListNode(0);
+        expected = new ListNode(0);
+        actual = target.addTwoNumbers(l1, l2);
+        assertTrue(LinkedListUtils.isLinkedListEqual(expected, actual));
+
+        l1 = new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9,
+                new ListNode(9, new ListNode(9)))))));
+        l2 = new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9))));
+        expected = new ListNode(0, new ListNode(9, new ListNode(9, new ListNode(9,
+                new ListNode(0, new ListNode(0, new ListNode(0, new ListNode(1))))))));
+        actual = target.addTwoNumbers(l1, l2);
+        assertTrue(LinkedListUtils.isLinkedListEqual(expected, actual));
+    }
+}
