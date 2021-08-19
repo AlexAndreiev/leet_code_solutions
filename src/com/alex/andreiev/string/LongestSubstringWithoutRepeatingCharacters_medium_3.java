257cdc26caebcd6cc79788d6c8f9bd119fdbef06
Oleksand Andreiev <andreyew89gmail.com>
2021-08-19 13:04:13 +0300

    LongestSubstringWithoutRepeatingCharacters_medium_3 and LongestSubstringWithoutRepeatingCharacters_medium_3Test.java have been added

---
+++ src/com/alex/andreiev/string/LongestSubstringWithoutRepeatingCharacters_medium_3.java
@@ -0,0 +1,40 @@
+package com.alex.andreiev.string;
+
+/*
+ Given a string s, find the length of the longest substring without repeating characters.
+
+ Constraints:
+    0 <= s.length <= 5 * 104
+    s consists of English letters, digits, symbols and spaces.
+* */
+
+import java.util.HashMap;
+
+public class LongestSubstringWithoutRepeatingCharacters_medium_3 {
+
+    public int lengthOfLongestSubstring(String s) {
+/* Approach 1
+* Time complexity - O(n)
+* Space complexity - O(1)
+* */
+        if (s == null) return 0;
+        int strLen = s.length();
+        if (strLen < 2) return strLen;
+
+        var hashMap = new HashMap<Character, Integer>();
+        int len = 0;
+        int i = 0;
+        int startPos = 0;
+        while (i < strLen) {
+            var ch = s.charAt(i);
+            if (hashMap.containsKey(ch) && hashMap.get(ch) >= startPos) {
+                if (len < (i - startPos))
+                    len = i - startPos;
+                startPos = hashMap.get(ch)+1;
+            }
+            hashMap.put(ch, i);
+            i++;
+        }
+        return Math.max(len, strLen - startPos);
+    }
+}
---
+++ test/com/alex/andreiev/string/LongestSubstringWithoutRepeatingCharacters_medium_3Test.java
@@ -0,0 +1,30 @@
+package com.alex.andreiev.string;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LongestSubstringWithoutRepeatingCharacters_medium_3Test {
+
+    @Test
+    void lengthOfLongestSubstring() {
+        var target = new LongestSubstringWithoutRepeatingCharacters_medium_3();
+        assertEquals(2, target.lengthOfLongestSubstring("abba"));
+        assertEquals(2, target.lengthOfLongestSubstring("cdd"));
+        assertEquals(1, target.lengthOfLongestSubstring("aa"));
+        assertEquals(2, target.lengthOfLongestSubstring("au"));
+        assertEquals(1, target.lengthOfLongestSubstring(" "));
+
+        assertEquals(3, target.lengthOfLongestSubstring("abcabcbb"));
+//         Explanation: The answer is "abc", with the length of 3.
+
+        assertEquals(1, target.lengthOfLongestSubstring("bbbbb"));
+//        Explanation: The answer is "b", with the length of 1.
+
+        assertEquals(3, target.lengthOfLongestSubstring("pwwkew"));
+//        Explanation: The answer is "wke", with the length of 3.
+//        Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+
+        assertEquals(0, target.lengthOfLongestSubstring(""));
+    }
+}
