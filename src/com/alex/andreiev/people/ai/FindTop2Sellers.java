8673d09be6e56aeff2709e7b0029ca41f2fed6af
Oleksandr Andreiev <andreyew89@gmail.com>
2024-05-04 10:09:19 +0300

    FindTop2Sellers solution has been added

---
+++ src/com/alex/andreiev/people/ai/FindTop2Sellers.java
@@ -0,0 +1,21 @@
+package com.alex.andreiev.people.ai;
+
+import java.util.*;
+
+public class FindTop2Sellers {
+
+    public List<Trade> findTop2Sellers(List<Trade> trades) {
+        var map = new HashMap<String, Integer>();
+        for (var trade : trades) {
+            map.put(trade.name, map.getOrDefault(trade.name, 0) + trade.amount);
+        }
+
+        return map.entrySet().stream()
+            .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+            .limit(2)
+            .map(e -> new Trade(e.getKey(), e.getValue()))
+            .toList();
+    }
+
+    public record Trade(String name, Integer amount){};
+}
---
+++ test/com/alex/andreiev/people/ai/FindTop2SellersTest.java
@@ -0,0 +1,33 @@
+package com.alex.andreiev.people.ai;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FindTop2SellersTest {
+
+    @Test
+    void findTop2Sellers() {
+        var target = new FindTop2Sellers();
+
+        var input = List.of(
+            new FindTop2Sellers.Trade("Alice", 50),
+            new FindTop2Sellers.Trade("Oleh", 5),
+            new FindTop2Sellers.Trade("Alice", 10),
+            new FindTop2Sellers.Trade("Alex", 30),
+            new FindTop2Sellers.Trade("Alex", 80),
+            new FindTop2Sellers.Trade("Alice", 100),
+            new FindTop2Sellers.Trade("Oleh", 3),
+            new FindTop2Sellers.Trade("Kate", 70)
+        );
+
+        var expected = List.of(
+            new FindTop2Sellers.Trade("Alice", 160),
+            new FindTop2Sellers.Trade("Alex", 110)
+        );
+
+        assertIterableEquals(expected, target.findTop2Sellers(input));
+    }
+}
