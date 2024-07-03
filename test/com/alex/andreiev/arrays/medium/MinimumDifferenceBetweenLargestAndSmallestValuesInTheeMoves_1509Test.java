73fa1f96a4829cdedd81bcabc1801da875a3dbe4
Oleksandr Andreiev <andreyew89@gmail.com>
2024-07-03 08:55:58 +0300

    MinimumDifferenceBetweenLargestAndSmallestValuesInTheeMoves_1509 solution has been added

--- test/com/alex/andreiev/arrays/medium/MinimumDifferenceBetweenLargestAndSmallestValuesInTheeMoves_1509Test.java
+++ test/com/alex/andreiev/arrays/medium/MinimumDifferenceBetweenLargestAndSmallestValuesInTheeMoves_1509Test.java
@@ -9,8 +9,8 @@ class MinimumDifferenceBetweenLargestAndSmallestValuesInTheeMoves_1509Test {
     @Test
     void minDifference() {
         var target = new MinimumDifferenceBetweenLargestAndSmallestValuesInTheeMoves_1509();
-//        assertEquals(0, target.minDifference(new int[]{5,3,2,4}));
+        assertEquals(0, target.minDifference(new int[]{5,3,2,4}));
         assertEquals(1, target.minDifference(new int[]{1,5,0,10,14}));
-//        assertEquals(0, target.minDifference(new int[]{3,100,20}));
+        assertEquals(0, target.minDifference(new int[]{3,100,20}));
     }
 }
